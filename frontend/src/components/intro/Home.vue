4260c54181511cc549fd8177aa7f0c0adb81731b
TAEJIN KIM <taejin0323@gmail.com>
2021-03-23 15:59:07 +0900

    feat: 🎸 contact form UI
    home 페이지의 contact UI 구현, 통신은.... 있다가...

---
+++ frontend/src/components/intro/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <div style="height:300px">
+      <h1>SAYE</h1>
+
+      <h3>Sing About Your Emotion</h3>
+
+      <v-divider dark></v-divider>
+      <p style="margin-top:20px">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quia
+        neque. Voluptas id nulla qui inventore reiciendis iusto maiores
+        distinctio?
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.about {
+  background-color: #3f3f3f;
+}
+</style>
--- frontend/src/views/IntroPage.vue
+++ frontend/src/views/IntroPage.vue
@@ -8,16 +8,20 @@
     </v-col>
     <v-col class="rightSide">
       <!-- Right side -->
-      <v-container fill-height>
-        <v-row align="center">
-          <Navbar />
-        </v-row>
-
+      <v-container fill-height pt-10>
+        <v-tabs v-model="tab" fixed-tabs dark background-color="#3f3f3f">
+          <v-tab v-for="item in items" :key="item.tab">
+            {{ item.tab }}
+          </v-tab>
+        </v-tabs>
         <transition appear @before-enter="beforeEnter" @enter="enter">
-          <TextAboutPage />
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="item in items" :key="item.tab">
+              <component v-bind:is="item.content"></component>
+            </v-tab-item>
+          </v-tabs-items>
         </transition>
-
-        <v-col align="center">
+        <v-row justify="center">
           <v-btn
             color="white accent-2"
             :to="{ name: 'HomePage' }"
@@ -29,7 +33,7 @@
             </v-icon>
             Start Now
           </v-btn>
-        </v-col>
+        </v-row>
       </v-container>
     </v-col>
   </v-container>
@@ -38,17 +42,24 @@
 <script>
 import gsap from "gsap";
 
-import TextAboutPage from "@/components/intro/TextAboutPage";
-import Navbar from "@/components/intro/Navbar";
+import Home from "@/components/intro/Home";
+import Contact from "@/components/intro/Contact";
 
 export default {
   components: {
-    TextAboutPage,
-    Navbar
+    Home,
+    Contact
   },
   data() {
     return {
-      logo: require("@/assets/saye_logo.jpg")
+      logo: require("@/assets/saye_logo.jpg"),
+      tab: 0,
+      items: [
+        { tab: "Home", content: "Home" },
+        { tab: "About Us", content: "" },
+        { tab: "Our Services", content: "" },
+        { tab: "Contact", content: "Contact" }
+      ]
     };
   },
   methods: {
