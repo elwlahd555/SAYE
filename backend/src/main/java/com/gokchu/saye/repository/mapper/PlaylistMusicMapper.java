fc972054771608b018d6a4311d1d18932ee7a52b
김지현 <khyun7621@naver.com>
2021-03-25 20:59:58 +0900

    fix: 플레이리스트 내 음원 작업 수정 (C,R,D 확인)

--- backend/src/main/java/com/gokchu/saye/repository/mapper/PlaylistMusicMapper.java
+++ backend/src/main/java/com/gokchu/saye/repository/mapper/PlaylistMusicMapper.java
@@ -10,7 +10,7 @@ import com.gokchu.saye.repository.dto.PlaylistMusic;
 @Mapper
 public interface PlaylistMusicMapper {
 	/* C :: 플레이리스트 안에 노래 추가 */
-	public int insertPlaylistMusic(int mNo, int pNo) throws Exception;
+	public int insertPlaylistMusic(int pNo, int mNo) throws Exception;
 	
 	/* R :: 플레이리스트 내 곡 조회 (플레이리스트 상세 조회) */
 	public List<Music> selectPlaylistMusic(int pNo) throws Exception;
