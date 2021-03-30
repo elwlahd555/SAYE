0ffceef70b8a2bd97b25dc48915531e3d053d059
goldeneastlight <goldeneastlight@gmail.com>
2021-03-30 14:05:35 +0900

    fix: 🎉 kakaoLogin JavaScript App key update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,7 @@ import vuetify from "./plugins/vuetify";
 import VueCookies from "vue-cookies";
 import VueYoutube from "vue-youtube";
 
-window.Kakao.init("c626463e9b79a84d5a8185d060679ef3");
+window.Kakao.init("443b00f34027675b5dffb8a2d9a6bdd7");
 
 Vue.config.productionTip = false;
 Vue.use(VueCookies);
