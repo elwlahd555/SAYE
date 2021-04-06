b9bdc95d95b38bf6b40d04b067ba29dfa6aebf89
goldeneastlight <goldeneastlight@gmail.com>
2021-04-06 17:51:02 +0900

    fix: ⛓ url match fix

--- frontend/src/components/home/Slide2.vue
+++ frontend/src/components/home/Slide2.vue
@@ -60,7 +60,7 @@ export default {
             .then(music => {
               this.django.music = music.data.mId;
               // console.log(this.django);
-              axios.get(`${django_URL}/recommend?emotion=${this.django.emotion}&music_id=${this.django.music}&requestCnt=${this.django.requestCnt}`)
+              axios.get(`${django_URL}/recommend?emotion=${this.django.emotion}&musicId=${this.django.music}&requestCnt=${this.django.requestCnt}`)
               .then(albums => {
                 console.log(albums);
               })
